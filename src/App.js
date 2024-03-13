9c916f4e4fb572b96e1b822d2c37167c715506fb
nao9 <nn@nx0.net>
2024-03-13 10:22:25 +0900

    dp

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React12
+          Learn React123
         </a>
       </header>
     </div>
